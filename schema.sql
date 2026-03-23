49004a709b5785158f795308355fd8739fd91a81
Joni Priuska <jdpriuska@gmail.com>
2026-03-23 13:54:57 +0200

    Add new item form

--- schema.sql
+++ schema.sql
@@ -4,3 +4,18 @@ CREATE TABLE users (
     password_hash TEXT
 );
 
+CREATE TABLE games (
+  id INTEGER PRIMARY KEY,
+  name TEXT UNIQUE,
+  seed_length INTEGER,
+  seed_allowed TEXT
+);
+
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    seed TEXT,
+    description TEXT,
+    user_id INTEGER REFERENCES users,
+    game_id INTEGER REFERENCES games
+);
