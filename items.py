d8022b10ddecc91402f0a7b34eaee213fbdd4be2
Joni Priuska <jdpriuska@gmail.com>
2026-03-27 16:22:07 +0200

    Show item list/info

--- items.py
+++ items.py
@@ -6,4 +6,27 @@ def add_item(title, seed, description, user_id, game):
 
     sql = """INSERT INTO items (title, seed, description, user_id, game_id)
     VALUES (?, ?, ?, ?, ?)"""
-    db. execute(sql, [title, seed, description, user_id, game_id])
+    db.execute(sql, [title, seed, description, user_id, game_id])
+
+def get_items():
+    sql = "SELECT id, title, seed FROM items ORDER BY id DESC"
+
+    return db.query(sql)
+
+def get_item(item_id):
+    sql = """
+    SELECT
+        I.title,
+        I.seed,
+        I.description,
+        U.username,
+        G.name AS game
+    FROM
+        Items I, Users U, Games G
+    WHERE
+        I.id = ? AND
+        I.user_id = U.id AND
+        I.game_id = G.id
+    """
+
+    return db.query(sql, [item_id])[0]
