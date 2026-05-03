aefc5847c75e201f86562e6fa6ef15441cb27db1
Joni Priuska <jdpriuska@gmail.com>
2026-05-03 15:52:26 +0300

    Fix regex

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 secret_key = "18fd24bf6a2ad4dac04a33963db1c42f"
-regex_expressions = {
-    1: ".",
-    2: "^[a-zA-Z0-9]$",
-    3: "^[0-9]$"
-}
+regex_expressions = [
+    ".*",               #All uniconde
+    "^[a-zA-Z0-9]+$",    #Only letters and numbers
+    "^[0-9]+$"           #Only numbers
+]
